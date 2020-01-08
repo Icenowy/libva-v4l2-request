7f473c55680bfba97e5109175d41904a982c2ce9
Icenowy Zheng <icenowy@aosc.io>
2020-01-08 21:03:05 +0800

    image: only call untiling code on ARM 32-bit architecture
    As we have only untiling code for 32-bit ARM, only call the code on
    32-bit ARM. On other architectures just return an error here.
    
    Theortically this codepath is only needed for old Allwinner SoCs that
    are ARM 32-bit, so this shouldn't hurt most usage.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- src/image.c
+++ src/image.c
@@ -156,11 +156,15 @@ static VAStatus copy_surface_to_image (struct request_data *driver_data,
 
 	for (i = 0; i < surface_object->destination_planes_count; i++) {
 		if (!video_format_is_linear(driver_data->video_format))
+#ifdef __arm__
 			tiled_to_planar(surface_object->destination_data[i],
 					buffer_object->data + image->offsets[i],
 					image->pitches[i], image->width,
 					i == 0 ? image->height :
 						 image->height / 2);
+#else
+			return VA_STATUS_ERROR_INVALID_VALUE;
+#endif
 		else {
 			memcpy(buffer_object->data + image->offsets[i],
 			       surface_object->destination_data[i],
